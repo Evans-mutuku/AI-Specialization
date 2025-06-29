d818b9dc3a0fde9d363b304241839e3414449b10
Evans-mutuku <evansnyamai98@gmail.com>
2025-06-29 20:11:50 +0300

    Updating specialization week 5

--- WEEK 5/lesson4.py
+++ WEEK 5/lesson4.py
@@ -3,16 +3,11 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 
-# Convert text data to numerical features
 vectorizer = TfidfVectorizer()
 X = vectorizer.fit_transform(df['cleaned_review'])
 y = df['review']
 
-# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# Select the logistic regression model
 model = LogisticRegression()
-
-# Train the model
 model.fit(X_train, y_train)
