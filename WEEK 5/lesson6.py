2c1e209581d7fce8671c5d19873a8dc427e8efda
Evans-mutuku <evansnyamai98@gmail.com>
2025-06-29 20:10:33 +0300

    Updating specialization week 5

--- WEEK 5/lesson6.py
+++ WEEK 5/lesson6.py
@@ -1,23 +1,18 @@
-# Import necessary libraries
 from flask import Flask, request, jsonify
 import pickle
 
-# Save the model and vectorizer
 with open('model.pkl', 'wb') as f:
     pickle.dump(model, f)
 with open('vectorizer.pkl', 'wb') as f:
     pickle.dump(vectorizer, f)
 
-# Create a Flask app
 app = Flask(__name__)
 
-# Load the model and vectorizer
 with open('model.pkl', 'rb') as f:
     model = pickle.load(f)
 with open('vectorizer.pkl', 'rb') as f:
     vectorizer = pickle.load(f)
 
-# Define the prediction endpoint
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json['review']
@@ -26,6 +21,5 @@ def predict():
     prediction = model.predict(X)
     return jsonify({'sentiment': prediction[0]})
 
-# Run the Flask app
 if __name__ == '__main__':
     app.run(debug=True)
