f6b5da20510a1efe88e7dedc90994ca771661d4c
Evans-mutuku <evansnyamai98@gmail.com>
2025-06-29 20:11:14 +0300

    Updating specialization week 5

--- WEEK 5/lesson5.py
+++ WEEK 5/lesson5.py
@@ -4,7 +4,6 @@ from sklearn.metrics import classification_report, confusion_matrix
 # Make predictions on the test set
 y_pred = model.predict(X_test)
 
-# Evaluate the model
 print("Confusion Matrix:")
 print(confusion_matrix(y_test, y_pred))
 print("\nClassification Report:")
